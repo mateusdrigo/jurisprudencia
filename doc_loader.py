ca8a48a454fccfc4328929d347b807e85ab805b4
Mateus Drigo da Silva <mateus.drigo@gmail.com>
2024-10-28 00:24:33 -0300

    Add arquivos

--- doc_loader.py
+++ doc_loader.py
@@ -1,6 +1,6 @@
 # Arquivo "doc_loader.py"
 
-from langchain_community.document_loaders import PyPDFDirectoryLoader
+#from langchain_community.document_loaders import PyPDFDirectoryLoader
 from langchain_text_splitters import RecursiveCharacterTextSplitter
 from langchain.schema.document import Document
 from get_embedding_function import get_embedding_function
